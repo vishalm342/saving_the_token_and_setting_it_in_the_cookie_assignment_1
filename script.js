d543e60820f8fc575d3b2f42f0ece4d5e84595fd
vishalm342 <vishalm8656@gmail.com>
2025-04-16 12:21:49 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
-const decrypt = (token) => {
-  // return decoded payload
-}
+const JWT_SECRET = 'Vishal@14'; 
+const ENCRYPTION_KEY = crypto.randomBytes(32);
+const IV = crypto.randomBytes(16);
+
+
+const encrypt = (payload) => {
+  
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
+ 
+  const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, IV);
+  let encrypted = cipher.update(token, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+
+  return {
+    encryptedToken: encrypted,
+    iv: IV.toString('hex')
+  };
+};
+
+
+const decrypt = ({ encryptedToken, iv }) => {
+  const decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    ENCRYPTION_KEY,
+    Buffer.from(iv, 'hex')
+  );
+
+  let decrypted = decipher.update(encryptedToken, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+
+
+  const decoded = jwt.verify(decrypted, JWT_SECRET);
+  return decoded;
+};
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
